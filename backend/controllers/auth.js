e83e460be4a835bc3d790225bfaa1443af56747e
Shrawani Medankar <shrawanimedankar@gmail.com>
2026-09-08 19:55:38 +0530

    Authentication done

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,24 +1,52 @@
+const bcrypt = require("bcrypt");
+const { UserModel } = require("../model/User");
 const { sendResponse } = require("../utils/sendResponse");
 
 const signup = async (req, res) => {
-  try{
-    const{fullName, email, password} = req.body;
+  try {
+    const { fullName, email, password } = req.body;
 
-    const existingUser = await UserModel.findOne({email});
+    const existingUser = await UserModel.findOne({ email });
 
-    if(existingUser){
+    if (existingUser) {
       return sendResponse(res, {
-        success:false,
-        status_code:400,
-        message:"Email already registered",
+        success: false,
+        status_code: 400,
+        message: "Email already registered",
       });
     }
 
-    const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const newUser = new UserModel({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
 
+    await newUser.save();
+
+    return sendResponse(res, {
+      success: true,
+      status_code: 201,
+      message: "Account created successfully",
+      data: {
+        fullName: newUser.fullName,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return sendResponse(res, {
+      success: false,
+      status_code: 500,
+      message: "Something went wrong",
+      error,
+    });
   }
 };
+
 const login = async (req, res) => {};
 
 module.exports = {
