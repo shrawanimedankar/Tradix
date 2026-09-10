e8b86cb65dd0ac64c890dbb90b2da81ed2e310f4
Shrawani Medankar <shrawanimedankar@gmail.com>
2026-09-10 20:27:28 +0530

    split routes and controllers

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,11 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
-
 const { getAllOrders, newOrder } = require("../controllers/orders");
 
 router.get("/allOrders", getAllOrders);
-
 router.post("/newOrder", newOrder);
 
 module.exports = router;
