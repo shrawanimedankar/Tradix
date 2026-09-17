42c0c9c77a2101cc3963e2a368d4541b76919326
Shrawani Medankar <shrawanimedankar@gmail.com>
2026-09-17 18:27:59 +0530

    Made Watchlist  Responsive

--- frontend/src/LandingPage/OpenAccount.jsx
+++ frontend/src/LandingPage/OpenAccount.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function OpenAccount() {
   return (
@@ -15,6 +16,9 @@ function OpenAccount() {
           </p>
 
           <button className="cutom-button">Sign up Now</button>
+          <Link to="/signup">
+            <button className="cutom-button">Sign up Now</button>
+          </Link>
         </div>
       </div>
     </div>
